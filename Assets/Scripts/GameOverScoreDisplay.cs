8136e32c69080107ffdd0878c5559f0c60fa7886
icat-PC\icat <575713113@qq.com>
2017-03-01 13:23:18 +0800

    commit init

---
+++ Assets/Scripts/GameOverScoreDisplay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameOverScoreDisplay : MonoBehaviour {
+
+    ScoreAbsorber scoreAbsorber;
+    Text text;
+
+    void Start()
+    {
+        scoreAbsorber = GameObject.FindObjectOfType<ScoreAbsorber>();
+        text = this.GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        text.text = "" + scoreAbsorber.score;
+    }
+}
---
+++ Assets/Scripts/QuitButton.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitButton : MonoBehaviour {
+
+    public int gazeTime = 2;
+    private LevelManager levelManager;
+
+    void Start()
+    {
+        SetGazedAt(false);
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+
+    public void SetGazedAt(bool gazedAt)
+    {
+        if (levelManager)
+        {
+            //print("yay Yes");
+            Invoke("OnGazeLoadScene", gazeTime);
+        }
+        else
+        {
+            //print("say No");
+        }
+
+    }
+
+    private void OnGazeLoadScene()
+    {
+        levelManager.Quit();
+    }
+
+    public void OnGazeEnter()
+    {
+        SetGazedAt(true);
+    }
+
+    public void OnGazeExit()
+    {
+        SetGazedAt(false);
+        CancelInvoke();
+    }
+
+}
