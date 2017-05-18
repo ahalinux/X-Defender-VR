e83d346ee193d05248053c633a98f009b9cb7077
icat-PC\icat <575713113@qq.com>
2017-05-18 22:20:26 +0800

    poolmanager

--- Assets/Scripts/BulletSpawner.cs
+++ Assets/Scripts/BulletSpawner.cs
@@ -6,10 +6,15 @@ public class BulletSpawner : MonoBehaviour {
     public GameObject ballPrefab;
     public float ballSpeed = 5.0f;
     public Vector3 bulletSpawnerPosition;
-    //public GameObject bulletSpawner;    //test
-
     public float spawnInterval = 2.0f;
+
     private float nextSpawnTime = 0;
+    private PoolManager poolManager;
+
+    void Start()
+    {
+        poolManager = GameObject.FindObjectOfType<PoolManager>();
+    }
 
     void Update()
     {
@@ -23,7 +28,6 @@ public class BulletSpawner : MonoBehaviour {
     private void BulletShoot()
     {
         GameObject instance = Instantiate(ballPrefab);
-        //instance.transform.position = this.transform.position;
         instance.transform.position = bulletSpawnerPosition;
         Rigidbody rb = instance.GetComponent<Rigidbody>();
         Camera camera = this.GetComponentInChildren<Camera>();
@@ -32,9 +36,7 @@ public class BulletSpawner : MonoBehaviour {
 
     private void BulletShootTest()
     {
-        //GameObject instance = Instantiate(ballPrefab, this.transform.position, this.transform.rotation) as GameObject;
         GameObject instance = Instantiate(ballPrefab, bulletSpawnerPosition, this.transform.rotation) as GameObject;
-
         instance.GetComponent<Rigidbody>().velocity = new Vector3(-30, 10, 0);
     }
 }
--- Assets/Scripts/PoolManager.cs
+++ Assets/Scripts/PoolManager.cs
@@ -1,15 +1,50 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 using System.Collections;
 
 public class PoolManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public int Capacity = 10;
+    private List<GameObject> dormantObjects = new List<GameObject>();
+
+    public GameObject Spawn(GameObject go)
+    {
+        GameObject temp = null;
+        if (dormantObjects.Count > 0)
+        {
+            foreach (GameObject dob in dormantObjects)
+            {
+                if (dob.name == go.name)
+                {
+                    temp = dob;
+                    dormantObjects.Remove(temp);
+                    return temp;
+                }
+            }
+        }
+
+        temp = GameObject.Instantiate(go) as GameObject;
+        temp.name = go.name;
+        return temp;
+    }
+
+    // 将用完的GameObject放入dormantObjects中
+    public void Despawn(GameObject go)
+    {
+        go.transform.parent = this.transform;
+        go.SetActive(false);
+        dormantObjects.Add(go);
+        Trim();
+    }
+
+    //FIFO 如果dormantObjects大于最大个数则将之前的GameObject都推出来。
+    public void Trim()
+    {
+        while (dormantObjects.Count > Capacity)
+        {
+            GameObject dob = dormantObjects[0];
+            dormantObjects.RemoveAt(0);
+            Destroy(dob);
+        }
+    }
 }
